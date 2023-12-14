bbc9e66a12a65ccd81a7ddbf4c329961f656f41b
Nicolas Manjarres <nicolasmanjarresg@gmail.com>
2023-12-13 21:46:59 -0500

    se reordenan archivos

---
+++ index.js
@@ -0,0 +1,38 @@
+const navMenu = document.getElementById('nav-menu');
+const hamburguer = document.getElementById('hamburguer');
+
+hamburguer.addEventListener('click',() => {
+    navMenu.classList.toggle('responsive-dinamic');
+    /* if(hamburguer.src.split('img')[1] == '/hamburguesa.png'){
+        hamburguer.src = '../../img/cruz.png';
+    }
+    else{
+        hamburguer.src = '../../img/hamburguesa.png';
+    } */
+    switch(hamburguer.src.split('img')[1]){
+        case ('/cruz.png'):
+            cruz.src = '../../img/cruz.png';
+            break
+        case ('/hamburguer.png'):
+            hamburguer.src = '../../img/hamburguesa.png';
+            break
+    }
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (window.innerWidth > 768) {
+        navMenu.classList.add('responsive-dinamic');
+    }
+})
+
+window.addEventListener('load', ()=> {
+    const container = document.getElementById('container');
+    const planetsContainer = document.getElementById('planets-container');
+    const spinner = document.getElementById('spinner');
+
+    setTimeout(()=> {
+        spinner.style.display = 'none';
+        container.style.display = 'block';
+        planetsContainer.style.display = 'block';
+    },2000)
+})
